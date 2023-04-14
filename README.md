0ec253f623c6b635e1f8977f6155b895b5d1f28a
anchal-simform <anchal.s@simformsolutions.com>
2023-04-14 13:51:45 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# VT Create video editor app
+
+Generate video from the canvas which has images and text with images and also able to add audio file to it
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+
+import "./App.css";
+import FirstEditor from "./components/FirstEditor";
+import SecondEditor from "./components/SecondEditor";
+import ThirdEditor from "./components/ThirdEditor";
+
+function App() {
+  return (
+    <div className="App height_full">
+      <header className="">
+        <h1 className="text_center">Video Editor / converter</h1>
+        {/* First Editor */}
+        {/* <ImageEditor /> */}
+        {/* Second Editor */}
+        {/* Third Editor */}
+      </header>
+      <main className="height_full">
+        {/* <SecondEditor /> */}
+        <ThirdEditor />
+      </main>
+    </div>
+  );
+}
+
+export default App;
